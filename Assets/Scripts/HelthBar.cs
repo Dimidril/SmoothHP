856e505a8a716cfee975136979e5f785197e8b1a
Dmitry <Dimidril@users.noreply.github.com>
2020-12-25 15:57:21 +0300

    Initial commit

---
+++ Assets/Scripts/HelthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+[RequireComponent(typeof(Slider))]
+public class HelthBar : MonoBehaviour
+{
+    [SerializeField] private float _smoothTime = 2f;
+
+    private Slider _bar;
+
+    private void Awake()
+    {
+        _bar = GetComponent<Slider>();
+    }
+
+    public void SetBarValue(float value)
+    {
+        _bar.DOValue(value, _smoothTime).SetEase(Ease.Linear);
+    }
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private HelthBar _helthBar;
+    [SerializeField] private float _maxHelthPoints = 100f;
+    [SerializeField] private float _helthPoints = 50f;
+
+    private void Start()
+    {
+        _helthBar.SetBarValue(_helthPoints / _maxHelthPoints);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _helthPoints -= damage;
+        if (_helthPoints > _maxHelthPoints)
+        {
+            _helthPoints = _maxHelthPoints;
+        }
+        else if(_helthPoints < 0)
+        {
+            _helthPoints = 0;
+            Die();
+        }
+        _helthBar.SetBarValue(_helthPoints / _maxHelthPoints);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
